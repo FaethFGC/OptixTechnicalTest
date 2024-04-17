16be16b5f74cb64b1e708d5250281cdb669ae9cc
Ren Bell <joebell2312@gmail.com>
2024-04-17 19:29:23 +0100

    Scaffolded tables

---
+++ Data/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OptixTechnicalTest.Data.Models
+{
+    public partial class Movie
+    {
+        public string? ReleaseDate { get; set; }
+        public string? Title { get; set; }
+        public string? Overview { get; set; }
+        public double? Popularity { get; set; }
+        public long? VoteCount { get; set; }
+        public double? VoteAverage { get; set; }
+        public string? OriginalLanguage { get; set; }
+        public string? Genre { get; set; }
+        public string? PosterUrl { get; set; }
+    }
+}
---
+++ Data/MoviesContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace OptixTechnicalTest.Data
+{
+    public partial class MoviesContext : DbContext
+    {
+        public MoviesContext()
+        {
+        }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("movies");
+
+                entity.Property(e => e.OriginalLanguage).HasColumnName("Original_Language");
+
+                entity.Property(e => e.PosterUrl).HasColumnName("Poster_Url");
+
+                entity.Property(e => e.ReleaseDate).HasColumnName("Release_Date");
+
+                entity.Property(e => e.VoteAverage).HasColumnName("Vote_Average");
+
+                entity.Property(e => e.VoteCount).HasColumnName("Vote_Count");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
