3d7e8ba652491dd7a193d4ee85ff701ac0800244
Ren Bell <joebell2312@gmail.com>
2024-04-17 19:56:39 +0100

    Added moviescontroller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OptixTechnicalTest.Data;
+using OptixTechnicalTest.Data.Models;
+using PagedList;
+
+namespace OptixTechnicalTest.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesContext _context;
+
+        public MoviesController(MoviesContext _context)
+        {
+            this._context = _context;
+        }
+
+        [HttpGet]
+        [Route("MoviesByName")]
+        public List<Movie> GetMovies([FromQuery] string nameSearch, [FromQuery] string? genre, [FromQuery] int? page, [FromQuery] int? pageSize)
+        {
+            var searchString = $"%{string.Join("%", nameSearch.Split(" "))}%";
+            var result = this._context.Movies.Where(x => EF.Functions.Like(x.Title, searchString) && (string.IsNullOrEmpty(genre) || x.Genre.Contains(genre))).ToList();
+
+            return result.ToPagedList(page ?? 1, pageSize ?? 30).ToList();
+        }
+    }
+}
--- Data/Models/Movie.cs
+++ Data/Models/Movie.cs
@@ -5,14 +5,14 @@ namespace OptixTechnicalTest.Data.Models
 {
     public partial class Movie
     {
-        public string? ReleaseDate { get; set; }
-        public string? Title { get; set; }
-        public string? Overview { get; set; }
-        public double? Popularity { get; set; }
-        public long? VoteCount { get; set; }
-        public double? VoteAverage { get; set; }
-        public string? OriginalLanguage { get; set; }
-        public string? Genre { get; set; }
-        public string? PosterUrl { get; set; }
+        public string ReleaseDate { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Overview { get; set; } = string.Empty;
+        public double Popularity { get; set; }
+        public long VoteCount { get; set; }
+        public double VoteAverage { get; set; }
+        public string OriginalLanguage { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string PosterUrl { get; set; } = string.Empty;
     }
 }
